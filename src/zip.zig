9c64ce0d5f027a834fb68f3cbd874fbaa824478f
Michael Wolf <susonicth@gmail.com>
2024-01-11 01:20:55 +0100

    try to implement filetime to zip time

--- src/zip.zig
+++ src/zip.zig
@@ -61,6 +61,8 @@ const ZipUdata = struct {
         return 1;
     }
 
+    const cTime = @cImport(@cInclude("time.h"));
+
     fn add_file(lua: *Lua) i32 {
         const ud: *ZipUdata = luax.getUserData(lua, name, ZipUdata);
         const source_name = filesystem.get_path_index(lua, 2);
@@ -74,32 +76,41 @@ const ZipUdata = struct {
         const comment: ?[*:0]const u8 = null;
         const compression: i32 = 0;
 
+        const stat = file.stat() catch luax.raiseFormattedError(lua, "could not open get file stats for '%s'", .{source_name.ptr});
+        const mtime: c_longlong = @intCast(stat.mtime);
+        const tm = cTime.localtime(&mtime);
+
         //var s: c.struct_stat = undefined;
         //_ = c.stat(source_name, &s);
-        //        const tm: c.tm = c.localtime(s.st_mtime);
-        //
-        //        var zfi: c.zip_fileinfo = .{
-        //            .tmz_date = .{
-        //                .tm_sec = tm.tm_sec,
-        //                .tm_min = tm.tm_min,
-        //                .tm_hour = tm.tm_hour,
-        //                .tm_mday = tm.tm_mday,
-        //                .tm_mon = tm.tm_mon,
-        //                .tm_year = tm.tm_year,
+        //const tm: c.tm = c.localtime(s.st_mtime);
 
         var zfi: c.zip_fileinfo = .{
             .tmz_date = .{
-                .tm_sec = 31,
-                .tm_min = 10,
-                .tm_hour = 12,
-                .tm_mday = 2,
-                .tm_mon = 3,
-                .tm_year = 1980,
+                .tm_sec = *tm.tm_sec,
+                .tm_min = tm.tm_min,
+                .tm_hour = tm.tm_hour,
+                .tm_mday = tm.tm_mday,
+                .tm_mon = tm.tm_mon,
+                .tm_year = tm.tm_year,
             },
             .dosDate = 0,
             .internal_fa = 0,
             .external_fa = 0,
         };
+
+        //var zfi: c.zip_fileinfo = .{
+        //    .tmz_date = .{
+        //        .tm_sec = 31,
+        //        .tm_min = 10,
+        //        .tm_hour = 12,
+        //        .tm_mday = 2,
+        //        .tm_mon = 3,
+        //        .tm_year = 1980,
+        //    },
+        //    .dosDate = 0,
+        //    .internal_fa = 0,
+        //    .external_fa = 0,
+        //};
 
         if (c.zipOpenNewFileInZip(ud.zfh, destination_name, &zfi, null, 0, null, 0, comment, c.Z_DEFLATED, compression) != c.ZIP_OK) {
             luax.raiseFormattedError(lua, "could not create file in zip '%s", .{destination_name.ptr});
