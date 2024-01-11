d072e4993baa798b87a973879f6252f44eef639e
Michael Wolf <susonicth@gmail.com>
2024-01-11 01:35:01 +0100

    try to implement filetime to zip time

--- src/zip.zig
+++ src/zip.zig
@@ -12,6 +12,7 @@ const allocator = std.heap.c_allocator;
 const c = @cImport({
     @cInclude("zip.h");
 });
+const cTime = @cImport(@cInclude("time.h"));
 
 const filesystem = @import("filesystem.zig");
 
@@ -61,8 +62,6 @@ const ZipUdata = struct {
         return 1;
     }
 
-    const cTime = @cImport(@cInclude("time.h"));
-
     fn add_file(lua: *Lua) i32 {
         const ud: *ZipUdata = luax.getUserData(lua, name, ZipUdata);
         const source_name = filesystem.get_path_index(lua, 2);
@@ -77,40 +76,9 @@ const ZipUdata = struct {
         const compression: i32 = 0;
 
         const stat = file.stat() catch luax.raiseFormattedError(lua, "could not open get file stats for '%s'", .{source_name.ptr});
-        const mtime: c_longlong = @intCast(stat.mtime);
-        const tm = cTime.localtime(&mtime);
+        var zfi: c.zip_fileinfo = undefined;
 
-        //var s: c.struct_stat = undefined;
-        //_ = c.stat(source_name, &s);
-        //const tm: c.tm = c.localtime(s.st_mtime);
-
-        var zfi: c.zip_fileinfo = .{
-            .tmz_date = .{
-                .tm_sec = *tm.tm_sec,
-                .tm_min = tm.tm_min,
-                .tm_hour = tm.tm_hour,
-                .tm_mday = tm.tm_mday,
-                .tm_mon = tm.tm_mon,
-                .tm_year = tm.tm_year,
-            },
-            .dosDate = 0,
-            .internal_fa = 0,
-            .external_fa = 0,
-        };
-
-        //var zfi: c.zip_fileinfo = .{
-        //    .tmz_date = .{
-        //        .tm_sec = 31,
-        //        .tm_min = 10,
-        //        .tm_hour = 12,
-        //        .tm_mday = 2,
-        //        .tm_mon = 3,
-        //        .tm_year = 1980,
-        //    },
-        //    .dosDate = 0,
-        //    .internal_fa = 0,
-        //    .external_fa = 0,
-        //};
+        getFileInfo(&stat, &zfi);
 
         if (c.zipOpenNewFileInZip(ud.zfh, destination_name, &zfi, null, 0, null, 0, comment, c.Z_DEFLATED, compression) != c.ZIP_OK) {
             luax.raiseFormattedError(lua, "could not create file in zip '%s", .{destination_name.ptr});
@@ -131,3 +99,29 @@ const ZipUdata = struct {
         return 1;
     }
 };
+
+fn getFileInfo(stat: *const fs.File.Stat, zfi: *c.zip_fileinfo) void {
+    if (builtin.os.tag == .windows) {
+        zfi.tmz_date.tm_sec = 31;
+        zfi.tmz_date.tm_min = 10;
+        zfi.tmz_date.tm_hour = 12;
+        zfi.tmz_date.tm_mday = 2;
+        zfi.tmz_date.tm_mon = 3;
+        zfi.tmz_date.tm_year = 1980;
+        zfi.dosDate = 0;
+        zfi.internal_fa = 0;
+        zfi.external_fa = 0;
+    } else {
+        const mtime: c_longlong = @intCast(stat.mtime);
+        const tm = cTime.localtime(&mtime);
+        zfi.tmz_date.tm_sec = tm.tm_sec;
+        zfi.tmz_date.tm_min = tm.tm_min;
+        zfi.tmz_date.tm_hour = tm.tm_hour;
+        zfi.tmz_date.tm_mday = tm.tm_mday;
+        zfi.tmz_date.tm_mon = tm.tm_mon;
+        zfi.tmz_date.tm_year = tm.tm_year;
+        zfi.dosDate = 0;
+        zfi.internal_fa = 0;
+        zfi.external_fa = 0;
+    }
+}
