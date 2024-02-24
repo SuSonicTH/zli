0e021286c5252098eadc004432ba2e496f2d58bc
Michael Wolf <susonicth@gmail.com>
2024-02-24 05:37:06 +0100

    cleaned up auxiliary.lua

--- src/auxiliary.lua
+++ src/auxiliary.lua
@@ -1,5 +1,6 @@
 local serpent = require "serpent"
 
+--[[ Globals ]]
 function arg_error(func, narg, message, level)
     error("bad argument #" .. narg .. " to " .. func .. " (" .. message .. ")", level)
 end
@@ -43,189 +44,7 @@ function sorted_pairs(tbl, func)
     end
 end
 
-local function io_read_lines(filename)
-    local ret = {}
-    for line in io.lines(filename) do
-        table.insert(ret, line)
-    end
-    return ret
-end
-
-local function io_write_lines(filename, tbl, eol)
-    eol = eol or "\n"
-    local fh = assert(io.open(filename, "w"))
-    for _, line in ipairs(tbl) do
-        fh:write(line, eol)
-    end
-    fh:close()
-end
-
-local function io_read_file(filename)
-    local fh = assert(io.open(filename, "r"))
-    local str = fh:read("a")
-    fh:close()
-    return str
-end
-
-local function io_write_file(filename, data)
-    local fh = assert(io.open(filename, "w+"))
-    fh:write(data)
-    fh:close()
-end
-
-local function io_append_file(filename, data)
-    local fh = assert(io.open(filename, "a+"))
-    fh:write(data)
-    fh:close()
-end
-
-local function table_copy(orig, copies)
-    copies = copies or {}
-    if type(orig) == 'table' then
-        if copies[orig] then
-            return copies[orig]
-        else
-            local copy = {}
-            copies[orig] = copy
-            for orig_key, orig_value in next, orig, nil do
-                copy[table_copy(orig_key, copies)] = table_copy(orig_value, copies)
-            end
-            setmetatable(copy, table_copy(getmetatable(orig), copies))
-            return copy
-        end
-    else
-        return orig
-    end
-end
-
-local function table_tostring(tbl, name)
-    arg_check_type("table.tostring", 1, tbl, 'table')
-    arg_check_type("table.tostring", 2, name, 'string', 'nil')
-
-    local str = serpent.block(tbl, {
-        sortkeys = true,
-        indent = "    ",
-        --sparse = true,
-        compact = false,
-        comment = false,
-        nocode = true
-    })
-    if name then
-        return name .. " = " .. str
-    end
-    return str
-end
-
-local function table_print(tbl, name)
-    arg_check_type("table.print", 1, tbl, 'table')
-    arg_check_type("table.print", 2, name, 'string', 'nil')
-    print(table_tostring(tbl, name))
-end
-
-local function table_remove_if(tbl, func)
-    arg_check_type("table.remove", 1, tbl, 'table')
-    arg_check_type("table.remove", 2, func, 'function')
-    local remove = {}
-    for i, item in ipairs(tbl) do
-        if (func(item)) then
-            remove[#remove + 1] = i
-        end
-    end
-
-    for i, index in ipairs(remove) do
-        table.remove(tbl, index - i + 1)
-    end
-    return tbl
-end
-
-local function table_filter(tbl, func)
-    arg_check_type("table.filter", 1, tbl, 'table')
-    arg_check_type("table.filter", 2, func, 'function')
-    local ret = {}
-    for i, item in ipairs(tbl) do
-        if (func(item)) then
-            ret[#ret + 1] = item
-        end
-    end
-    return ret
-end
-
-local function table_add_all(tbl, ...)
-    arg_check_type("table.add_all", 1, tbl, 'table')
-    for _, arg in ipairs { ... } do
-        if type(arg) == 'table' then
-            for _, item in ipairs(arg) do
-                tbl[#tbl + 1] = item
-            end
-        else
-            tbl[#tbl + 1] = arg
-        end
-    end
-    return tbl
-end
-
-local function table_insert_all(tbl, index, ...)
-    arg_check_type("table.insert_all", 1, tbl, 'table')
-    arg_check_type("table.insert_all", 2, index, 'number')
-    for _, arg in ipairs { ... } do
-        if type(arg) == 'table' then
-            for _, item in ipairs(arg) do
-                table.insert(tbl, index, item)
-                index = index + 1
-            end
-        else
-            table.insert(tbl, index, arg)
-            index = index + 1
-        end
-    end
-    return tbl
-end
-
-local function table_load_file(filename, options)
-    arg_check_type("table.load_file", 1, filename, 'string')
-    return serpent.load(io_read_file(filename), options)
-end
-
-local function default_index(self, key)
-    local value = self.tbl[key]
-    if value ~= nil then
-        return value
-    end
-    value = self.default[key]
-
-    if value == nil and rawget(self, "error") then
-        local error_type = type(self.error)
-        if error_type == 'function' then
-            return self.error(self.tbl, key)
-        elseif error_type == 'string' then
-            error(self.error:format(key), 2)
-        else
-            error(string.format("option '%s' is not set and is not optional", key, 2))
-        end
-    end
-
-    return value
-end
-
-local default_meta = { __index = default_index }
-
-local function default(tbl, default, error)
-    return setmetatable({
-        tbl = tbl,
-        default = default,
-        error = error
-    }, default_meta)
-end
-
-local function get_home()
-    local home = os.getenv("HOME")
-    if (home == nil and os.get_name() == "windows") then
-        home = os.getenv("homedrive") .. os.getenv("homepath")
-    end
-    return home
-end
-
-local function lambda(lambda_str)
+function lambda(lambda_str)
     arg_check_type("lambda / L / string:l()", 1, lambda_str, 'string')
 
     local firstChar = lambda_str:sub(1, 1)
@@ -260,7 +79,204 @@ local function lambda(lambda_str)
     end
 end
 
-local function starts_with(self, str)
+--[[ io ]]
+
+function io.read_lines(filename)
+    local ret = {}
+    for line in io.lines(filename) do
+        table.insert(ret, line)
+    end
+    return ret
+end
+
+function io.write_lines(filename, tbl, eol)
+    eol = eol or "\n"
+    local fh = assert(io.open(filename, "w"))
+    for _, line in ipairs(tbl) do
+        fh:write(line, eol)
+    end
+    fh:close()
+end
+
+function io.read_file(filename)
+    local fh = assert(io.open(filename, "r"))
+    local str = fh:read("a")
+    fh:close()
+    return str
+end
+
+function io.write_file(filename, data)
+    local fh = assert(io.open(filename, "w+"))
+    fh:write(data)
+    fh:close()
+end
+
+function io.append_file(filename, data)
+    local fh = assert(io.open(filename, "a+"))
+    fh:write(data)
+    fh:close()
+end
+
+--[[ table ]]
+
+function table.copy(orig, copies)
+    copies = copies or {}
+    if type(orig) == 'table' then
+        if copies[orig] then
+            return copies[orig]
+        else
+            local copy = {}
+            copies[orig] = copy
+            for orig_key, orig_value in next, orig, nil do
+                copy[table_copy(orig_key, copies)] = table_copy(orig_value, copies)
+            end
+            setmetatable(copy, table_copy(getmetatable(orig), copies))
+            return copy
+        end
+    else
+        return orig
+    end
+end
+
+function table.tostring(tbl, name)
+    arg_check_type("table.tostring", 1, tbl, 'table')
+    arg_check_type("table.tostring", 2, name, 'string', 'nil')
+
+    local str = serpent.block(tbl, {
+        sortkeys = true,
+        indent = "    ",
+        --sparse = true,
+        compact = false,
+        comment = false,
+        nocode = true
+    })
+    if name then
+        return name .. " = " .. str
+    end
+    return str
+end
+
+function table.print(tbl, name)
+    arg_check_type("table.print", 1, tbl, 'table')
+    arg_check_type("table.print", 2, name, 'string', 'nil')
+    print(table.tostring(tbl, name))
+end
+
+function table.remove_if(tbl, func)
+    arg_check_type("table.remove", 1, tbl, 'table')
+    arg_check_type("table.remove", 2, func, 'function')
+    local remove = {}
+    for i, item in ipairs(tbl) do
+        if (func(item)) then
+            remove[#remove + 1] = i
+        end
+    end
+
+    for i, index in ipairs(remove) do
+        table.remove(tbl, index - i + 1)
+    end
+    return tbl
+end
+
+function table.filter(tbl, func)
+    arg_check_type("table.filter", 1, tbl, 'table')
+    arg_check_type("table.filter", 2, func, 'function')
+    local ret = {}
+    for i, item in ipairs(tbl) do
+        if (func(item)) then
+            ret[#ret + 1] = item
+        end
+    end
+    return ret
+end
+
+function table.add_all(tbl, ...)
+    arg_check_type("table.add_all", 1, tbl, 'table')
+    for _, arg in ipairs { ... } do
+        if type(arg) == 'table' then
+            for _, item in ipairs(arg) do
+                tbl[#tbl + 1] = item
+            end
+        else
+            tbl[#tbl + 1] = arg
+        end
+    end
+    return tbl
+end
+
+function table.insert_all(tbl, index, ...)
+    arg_check_type("table.insert_all", 1, tbl, 'table')
+    arg_check_type("table.insert_all", 2, index, 'number')
+    for _, arg in ipairs { ... } do
+        if type(arg) == 'table' then
+            for _, item in ipairs(arg) do
+                table.insert(tbl, index, item)
+                index = index + 1
+            end
+        else
+            table.insert(tbl, index, arg)
+            index = index + 1
+        end
+    end
+    return tbl
+end
+
+function table.load_file(filename, options)
+    arg_check_type("table.load_file", 1, filename, 'string')
+    return serpent.load(io_read_file(filename), options)
+end
+
+local function default_index(self, key)
+    local value = self.tbl[key]
+    if value ~= nil then
+        return value
+    end
+    value = self.default[key]
+
+    if value == nil and rawget(self, "error") then
+        local error_type = type(self.error)
+        if error_type == 'function' then
+            return self.error(self.tbl, key)
+        elseif error_type == 'string' then
+            error(self.error:format(key), 2)
+        else
+            error(string.format("option '%s' is not set and is not optional", key, 2))
+        end
+    end
+
+    return value
+end
+
+local default_meta = { __index = default_index }
+
+function table.default(tbl, default, error)
+    return setmetatable({
+        tbl = tbl,
+        default = default,
+        error = error
+    }, default_meta)
+end
+
+--[[ os ]]
+function os.home()
+    local home = os.getenv("HOME")
+    if (home == nil and os.get_name() == "windows") then
+        home = os.getenv("homedrive") .. os.getenv("homepath")
+    end
+    return home
+end
+
+os.name = os.get_name()
+os.is_windows = os.get_name() == "windows"
+os.is_linux = os.get_name() == "linux"
+os.is_mac = os.get_name() == "macos"
+os.separator = os.get_name() == "windows" and "\\" or "/"
+
+--[[ string functions ]]
+
+string.L = lambda
+
+function string.starts_with(self, str)
     if self:sub(1, #str) == str then
         return true
     else
@@ -268,7 +284,7 @@ local function starts_with(self, str)
     end
 end
 
-local function ends_with(self, str)
+function string.ends_with(self, str)
     if self:sub(#str) == str then
         return true
     else
@@ -276,7 +292,7 @@ local function ends_with(self, str)
     end
 end
 
-local function contains(self, str)
+function string.contains(self, str)
     if self:find(#str, 1, true) then
         return true
     else
@@ -284,39 +300,7 @@ local function contains(self, str)
     end
 end
 
---setting global funcitons and constants
-io.read_lines = io_read_lines
-io.write_lines = io_write_lines
-io.read_file = io_read_file
-io.write_file = io_write_file
-io.append_file = io_append_file
-
-table.copy = table_copy
-table.tostring = table_tostring
-table.print = table_print
-table.dump = serpent.dump
-table.dump_line = serpent.line
-table.dump_block = serpent.block
-table.load = serpent.load
-table.load_file = table_load_file
-table.remove_if = table_remove_if
-table.filter = table_filter
-table.add_all = table_add_all
-table.insert_all = table_insert_all
-table.default = default
+--[[ math ]]
 
 math.maxdouble = 1.7976931348623158e+308
 math.mindouble = 2.2250738585072014e-308
-
-os.name = os.get_name()
-os.is_windows = os.get_name() == "windows"
-os.is_linux = os.get_name() == "linux"
-os.is_mac = os.get_name() == "macos"
-os.home = get_home()
-os.separator = os.get_name() == "windows" and "\\" or "/"
-
-string.starts_with = starts_with
-string.ends_with = ends_with
-string.contains = contains
-string.L = lambda
-L = lambda
