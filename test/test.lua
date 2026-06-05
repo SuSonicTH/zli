bbdc14318ba8def5b2539e8ca423990562ecf5ca
Michael Wolf <susonicth@gmail.com>
2026-06-05 11:43:44 +0200

    added a luastrip.file test; added temp directory with cleanup to tests

--- test/test.lua
+++ test/test.lua
@@ -1,9 +1,18 @@
-local lu = require 'luaunit'
+local lu = require "luaunit"
 local fs = require "filesystem"
 
 RUN_ALL = true
 
+--delete everything in ./test/temp except .gitignore
+for _, file in ipairs(fs.list("./test/temp")) do
+    if file.name ~= ".gitignore" then
+        file:delete()
+    end
+end
+
+--require all test_*.lua files in ./test/
 for fileName in sorted_pairs(fs.dir("./test/")) do
+    print(fileName)
     if fileName:find("test_.*%.lua") then
         require("test/" .. fileName:sub(1, -5))
     end
--- test/test_lustrip.lua
+++ test/test_lustrip.lua
@@ -9,8 +9,7 @@ function Test_luastrip.Test_string_one_liner()
     lu.assertEquals("local a=1", luastrip.string("\t local\t a = 1 \n"))
 end
 
-function Test_luastrip.Test_bigger_block()
-    local block = "\r\n" .. [=[
+local block = "\r\n" .. [=[
 
 -- Some bigger script
 
@@ -30,7 +29,21 @@ print ( 'result =\''  ..
 
     ]=]
 
-    lu.assertEquals("local a=1 local b=2 print('result =\\''..(a+b)..'\\'')", luastrip.string(block))
+local blockStripped = "local a=1 local b=2 print('result =\\''..(a+b)..'\\'')"
+
+function Test_luastrip.Test_bigger_block()
+    lu.assertEquals(blockStripped, luastrip.string(block))
+end
+
+function Test_luastrip.Test_file_stripping()
+    local source = "./test/temp/luastrip.in";
+    local stripped = "./test/temp/luastrip.out";
+
+    io.write_file(source, block)
+    luastrip.file(source, stripped)
+
+    local actual = io.read_file(stripped);
+    lu.assertEquals(blockStripped, actual)
 end
 
 if not RUN_ALL then
